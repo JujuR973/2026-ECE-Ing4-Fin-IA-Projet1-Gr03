6856ecdbd3086783e70bce70b76036af11ad6309
JujuR973 <juliette.sellito@edu.ece.fr>
2026-02-02 14:21:16 +0100

    projet 1.1

--- groupe_03_Juliette_Mathis_Market_Making/src/model_hjb.py
+++ groupe_03_Juliette_Mathis_Market_Making/src/model_hjb.py
@@ -6,5 +6,10 @@ def compute_optimal_spread(inventory, base_spread=1.0, alpha=0.1):
     base_spread : spread minimal
     alpha       : aversion au risque
     """
-    spread = base_spread + alpha * abs(inventory)
+    spread = base_spread + alpha * abs(inventory) 
+    """ abs = absolute value : permet de mesurer la distance à zéro de l'inventaire. 
+    Car plus ton q devient grand =distant de zéro augmente,
+    plus ton spread s'élargit 
+    (=Plus mon stock s'éloigne de zéro, plus j'augmente mon prix pour me protéger))"""
     return spread
+
